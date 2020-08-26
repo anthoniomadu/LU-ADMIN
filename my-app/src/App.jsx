9a03bb427009dc2ad1759d31bfe563a6b153d9bb
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-08-26 12:36:58 +0100

    updated stripped app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -76,15 +76,17 @@ import React from 'react';
 import { Admin, Resource,ListGuesser } from 'react-admin';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
-import { PostList } from './posts';
+import { PostList, PostEdit, PostCreate } from './posts';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 
 const App = () => (
        <Admin dataProvider={dataProvider}>
-            <Resource name="users" list={UserList} />
-            <Resource name="posts" list={PostList} />
+            <Resource name="users" list={UserList} icon={UserIcon}  />
+            <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}  />
         </Admin>
     );
 export default App;
